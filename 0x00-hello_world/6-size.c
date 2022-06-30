f154a41cd951ca2849c9b65c4c8b2c60e37d84c2
CephaMK <ebenezamk@gmail.com>
2022-06-30 16:56:02 -0700

    Add 6-sice.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ *
+ * return (0) always succesful
+ */
+int main (void)
+{
+	printf("Size of a char:%d%c\n Size of an int:%d%c\n Size of a long int:%d%c\n Size of a long long init:%d%c Size of a float:%d%c\n", 1'byte(s)',4'byte(s)',8'byte(s)',8'byte(s)',4'byte(s)');
+
+	return (0);
+}
