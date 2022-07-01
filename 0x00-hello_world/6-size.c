aaf13ba0905d35c50fb4d858fca1b7012f690d0d
CephaMK <ebenezamk@gmail.com>
2022-06-30 17:03:47 -0700

    trys

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main (void)
 {
-	printf("Size of a char:%d%c\n Size of an int:%d%c\n Size of a long int:%d%c\n Size of a long long init:%d%c Size of a float:%d%c\n", 1'byte(s)',4'byte(s)',8'byte(s)',8'byte(s)',4'byte(s)');
+	 printf(“size of a char: %zu byte(s)”, sizeof(char));
 
 	return (0);
 }
