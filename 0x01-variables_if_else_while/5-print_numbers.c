84d8981ebbba3caff4db340625505300788f12ec
CephaMK <ebenezamk@gmail.com>
2022-07-01 15:43:04 -0700

    add 5-print_numbers.c eith C program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -prints all single digit numbers of base 10 starting from 0
+ *
+ * return always 0
+ */
+int main(void)
+{
+	int mk;
+
+	for (mk = 0; mk < 10; mk++)
+		printf("%d\n", mk);
+
+	return (0);
+}
