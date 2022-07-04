f8e8e5b8b4e70095b6bc4304958a502692e44cc3
CephaMK <ebenezamk@gmail.com>
2022-07-04 07:07:54 -0700

    Add 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,13 @@
+/*
+ * File 102-print_comb5.c
+ */
+
 #include <stdio.h>
 
 /**
  * main - prints all possible combinations of two digit numbers
  *
- * return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
