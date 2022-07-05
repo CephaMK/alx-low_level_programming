9aeac2c581d36106ab717e861c63d8d04df39223
CephaMK <ebenezamk@gmail.com>
2022-07-05 12:57:51 -0700

    create file  0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,23 @@
  * File 0-putchar.c
  */
 
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints '_putchar' followed by a new line
+ * putcharc - prints "_putchar"
  *
- * Return: 0
+ * Return: nothing
  */
-int main(void)
+void putcharc(void)
 {
-	printf("_putchar\n");
-
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 }
+
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+/*
+ * File main.c
+ */
+
+#include "main.h"
+
+/**
+ * main - calls out function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putcharc();
+
+	return (0);
+}
