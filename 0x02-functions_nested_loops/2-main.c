bcd961e3c0477a74ea8485b8a5df111355b8ae38
CephaMK <ebenezamk@gmail.com>
2022-07-05 13:55:22 -0700

    Add file 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,17 @@
+/*
+ * File 2-main.c
+ */
+
+#include "main.h"
+
+/**
+ * main - calls out function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/*
+ * File 2-print_alphabet_x10.c
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet
+ * 			followed by a new line.
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int _3;
+	char v;
+
+	while (_3++ <= 9)
+	{
+		for (v = 'a'; v <= 'z'; v++)
+			_putchar(v);
+
+		_putchar('\n');
+	}
+}
