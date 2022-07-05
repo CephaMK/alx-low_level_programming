ce5d27f3be9800dcba6efbf1827e59e40a76d163
CephaMK <ebenezamk@gmail.com>
2022-07-05 10:15:04 -0700

    make new directory 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+ * File 0-putchar.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints '_putchar' followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+0x02-functions_nested_loop
+file with C programs.
+
+0-putchar.c
+Program that prints _putchar, followed by a new line.
