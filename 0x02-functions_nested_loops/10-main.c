d9a4a5943b94d591e6b65dc927d1f0ec83aee378
CephaMK <ebenezamk@gmail.com>
2022-07-06 10:15:29 -0700

    Add file 10-add.c

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,21 @@
+/*
+ * File 10-main.c
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls out function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89,9);
+	printf("%d\n", n);
+
+	return (0);
+}
