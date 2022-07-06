e0fc635ea48128083cb5d812b65eed650c55c58b
CephaMK <ebenezamk@gmail.com>
2022-07-06 05:00:04 -0700

    Add file 5-sign.c

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+/*
+ * File 3-main.c
+ */
+
+#include "main.h"
+
+/**
+ * main - calls out function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	xctr_islower((c >= 'a' && c <= 'z')int c);
+
+	return (0);
+}
