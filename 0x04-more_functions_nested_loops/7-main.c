8760d81f95182e434ea05de71e3741c47ad94bd5
CephaMK <ebenezamk@gmail.com>
2022-07-07 12:57:54 -0700

    add file 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,20 @@
+/*
+ * File 7-main.c
+ */
+
+#include "main.h"
+
+/**
+ * main - calls out function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+
+	return (0);
+}
