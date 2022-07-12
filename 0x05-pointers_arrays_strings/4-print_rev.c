2c87d6ce897886040e696ee8af67be6fb74d892a
CephaMK <ebenezamk@gmail.com>
2022-07-12 11:46:34 -0700

    Add file 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/*
+ * File 4-print_rev.c
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - prints strint in reverse followed by a new line
+ * @s: string in question
+ *
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int l = 0, index;
+
+	while (s[index++])
+		l++;
+	for (index = l - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
