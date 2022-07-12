2098196e7074dd5ec624706a0629a449d38e6001
CephaMK <ebenezamk@gmail.com>
2022-07-12 13:19:46 -0700

    Add file 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+/*
+ * File 100-atoi.c
+ */
+
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: strung in question
+ *
+ * Return: integer value of convverted string
+ */
+int _atoi(char *s)
+{
+	int sg = 1;
+	unsigned int n = 0;
+
+	do {
+		if (*s == '-')
+			sg *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+
+		else if (n > 0)
+			break;
+
+	} while (*s++);
+
+		return (n * sg);
+}
+
