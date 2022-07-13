024295393f01f15a9417697780f661b05475bc64
CephaMK <ebenezamk@gmail.com>
2022-07-13 09:57:20 -0700

    DIRECTORY 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+DRIRECTORY 0x06-pointers_arrays_strings
+
+POINTERS, ARRAYS AND STRINGS
+
+0-strcat.c
+function that concatenates two strings.
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,17 @@
+/*
+ * File _putchar.c
+ */
+
+#include <unistd.h>
+
+/**
+ * _putchar - prints character c to stdout
+ * @c: character to be printed
+ *
+ * Return: 1 on success
+ *	-1 on error and rectifies error
+ */
+int _putchar(char c)
+{
+	return (write(1 &c 1));
+}
